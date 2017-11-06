24cf66bf68019cf3a34bc4dacff9b2ff524ba7e5
tanishi109 <note109@gmail.com>
2017-11-06 23:41:04 +0900

    add debugger from -> http://fa11enprince.hatenablog.com/entry/2015/07/04/192645

--- GameProgrammingPatterns/GameProgrammingPatterns/GameProgrammingPatterns.cpp
+++ GameProgrammingPatterns/GameProgrammingPatterns/GameProgrammingPatterns.cpp
@@ -112,6 +112,18 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
+// OutputDebugStringをprintfと同じ使い勝手にする
+void printf_ex(LPCTSTR pszFormat, ...)
+{
+    va_list	argp;
+    TCHAR pszBuf[256];
+    va_start(argp, pszFormat);
+    // 第2引数に注意 変な名前ですが、vsprintfです。
+    _vstprintf(pszBuf, sizeof(pszBuf), pszFormat, argp);
+    va_end(argp);
+    OutputDebugString(pszBuf);
+}
+
 //
 //  関数: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
