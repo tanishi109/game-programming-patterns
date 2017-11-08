4cd747ce19d5f2730b215a826f3c5d37577116f5
tanishi109 <note109@gmail.com>
2017-11-08 13:27:12 +0900

    create Page & its State classes with handlers

--- GameProgrammingPatterns/GameProgrammingPatterns/GameProgrammingPatterns.cpp
+++ GameProgrammingPatterns/GameProgrammingPatterns/GameProgrammingPatterns.cpp
@@ -118,6 +118,119 @@ static const int SELECTOR_POS[1][2] = {
 };
 static const int SELECTOR_SIZE = 32;
 
+class SelectState;
+class FlyWeightState;
+class PageState
+{
+public:
+    virtual ~PageState() {}
+    virtual void handleInput(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {}
+    static SelectState select;
+    static FlyWeightState flyWeight;
+};
+
+class SelectState : public PageState
+{
+public:
+    SelectState() {}
+    virtual void handleInput(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+        switch (message)
+        {
+        case WM_LBUTTONDOWN:
+            {
+                const int mouse_x = GET_X_LPARAM(lParam);
+                const int mouse_y = GET_Y_LPARAM(lParam);
+
+                for (const int* s : SELECTOR_POS) {
+                    const int sel_x = *s;
+                    const int sel_y = *(s+1);
+
+                    if (
+                        (sel_x < mouse_x && mouse_x < sel_x + SELECTOR_SIZE) &&
+                        (sel_y < mouse_y && mouse_y < sel_y + SELECTOR_SIZE)
+                       ) {
+                       printf_ex(_T("*** CLICKED!"));
+                    }
+                }
+            }
+            break;
+        case WM_PAINT:
+            {
+                PAINTSTRUCT ps;
+                HDC hdc = BeginPaint(hWnd, &ps);
+
+                // 選択肢を描画
+                drawSelector(hWnd, ps);
+
+                EndPaint(hWnd, &ps);
+            }
+            break;
+        }
+    }
+private:
+    void drawSelector(HWND hWnd, PAINTSTRUCT ps)
+    {
+        // Obtain the size of the drawing area.
+        RECT rc;
+        GetClientRect(
+            hWnd,
+            &rc
+        );
+        // Save the original object
+        HGDIOBJ original = NULL;
+        original = SelectObject(
+            ps.hdc,
+            GetStockObject(DC_PEN)
+        );
+        // Create a pen.            
+        HPEN blackPen = CreatePen(PS_SOLID, 3, 0x333333);
+
+        SelectObject(ps.hdc, blackPen);
+        // Draw a rectangle.
+        for (const int* s: SELECTOR_POS) {
+            int x = *s;
+            int y = *(s+1);
+            Rectangle(
+                ps.hdc,
+                x,
+                y,
+                x + SELECTOR_SIZE,
+                y + SELECTOR_SIZE
+            );
+        }
+
+        DeleteObject(blackPen);
+        SelectObject(ps.hdc, original);
+    }
+};
+
+class FlyWeightState : public PageState
+{
+public:
+    FlyWeightState() {}
+    virtual void handleInput(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    }
+};
+
+SelectState PageState::select = *new SelectState();
+FlyWeightState PageState::flyWeight = *new FlyWeightState();
+
+class Page
+{
+public:
+    Page() {
+        state_ = &PageState::select;
+    }
+    virtual void handleInput(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+    {
+        state_->handleInput(hWnd, message, wParam, lParam);
+    }
+private:
+    PageState* state_;
+};
+
+Page* page = new Page();
+
 //
 //  関数: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
@@ -130,28 +243,10 @@ static const int SELECTOR_SIZE = 32;
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    page->handleInput(hWnd, message, wParam, lParam);
+
     switch (message)
     {
-    case WM_LBUTTONDOWN:
-        {
-            OutputDebugString(_T("left clicked!!"));
-
-            const int mouse_x = GET_X_LPARAM(lParam);
-            const int mouse_y = GET_Y_LPARAM(lParam);
-
-            for (const int* s : SELECTOR_POS) {
-                const int sel_x = *s;
-                const int sel_y = *(s+1);
-
-                if (
-                    (sel_x < mouse_x && mouse_x < sel_x + SELECTOR_SIZE) &&
-                    (sel_y < mouse_y && mouse_y < sel_y + SELECTOR_SIZE)
-                   ) {
-                   printf_ex(_T("*** CLICKED!"));
-                }
-            }
-        }
-        break;
     case WM_COMMAND:
         {
             int wmId = LOWORD(wParam);
@@ -169,17 +264,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
         }
         break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-
-            // 選択肢を描画
-            DrawSelector(hWnd, ps);
-
-            EndPaint(hWnd, &ps);
-        }
-        break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -187,41 +271,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
-}
-
-void DrawSelector(HWND hWnd, PAINTSTRUCT ps)
-{
-    // Obtain the size of the drawing area.
-    RECT rc;
-    GetClientRect(
-		hWnd,
-		&rc
-	);
-	// Save the original object
-	HGDIOBJ original = NULL;
-	original = SelectObject(
-		ps.hdc,
-		GetStockObject(DC_PEN)
-	);
-	// Create a pen.            
-	HPEN blackPen = CreatePen(PS_SOLID, 3, 0x333333);
-
-	SelectObject(ps.hdc, blackPen);
-	// Draw a rectangle.
-    for (const int* s: SELECTOR_POS) {
-        int x = *s;
-        int y = *(s+1);
-        Rectangle(
-            ps.hdc,
-            x,
-            y,
-            x + SELECTOR_SIZE,
-            y + SELECTOR_SIZE
-        );
-    }
-
-	DeleteObject(blackPen);
-	SelectObject(ps.hdc, original);
 }
 
 // バージョン情報ボックスのメッセージ ハンドラーです。
