82b478b161aff07dd1fd92135709f00dd2c5b9cf
tanishi109 <note109@gmail.com>
2017-11-11 16:08:03 +0900

    add flyWeight pattern

--- GameProgrammingPatterns/GameProgrammingPatterns/FlyWeightState.cpp
+++ GameProgrammingPatterns/GameProgrammingPatterns/FlyWeightState.cpp
@@ -3,6 +3,74 @@
 #include "Page.h"
 #include "SelectState.h"
 
+#include <random>
+#include <map>
+using namespace std;
+
+void World::generateTerrain()
+{
+    std::mt19937 mt{ std::random_device{}() };
+    std::uniform_int_distribution<int> dist(1, 10);
+
+    for (int x = 0; x < WIDTH; x++) {
+        for (int y = 0; y < HEIGHT; y++) {
+            if (dist(mt) == 10) {
+                tiles_[x][y] = &hillTerrain_;
+            } else {
+                tiles_[x][y] = &grassTerrain_;
+            }
+        }
+    }
+
+    int x = dist(mt);
+    for (int y = 0; y < HEIGHT; y++) {
+        tiles_[x][y] = &riverTerrain_;
+    }
+}
+
+void World::drawTerrain(HWND hWnd, PAINTSTRUCT ps)
+{
+    // Obtain the size of the drawing area.
+    RECT rc;
+    GetClientRect(
+        hWnd,
+        &rc
+    );
+    // Save the original object
+    HGDIOBJ original = NULL;
+    original = SelectObject(
+        ps.hdc,
+        GetStockObject(DC_PEN)
+    );
+    // Create a penMap.
+    map<int, HPEN> penMap;
+
+    // Draw world.
+    for (int x = 0; x < WIDTH; x++) {
+        for (int y = 0; y < HEIGHT; y++) {
+            Terrain* tile = tiles_[x][y];
+            int colorCode = tile->getColor();
+
+            if (!penMap[colorCode]) {
+                HPEN tilePen = CreatePen(PS_SOLID, 1, colorCode);
+
+                penMap[colorCode] = tilePen;
+            }
+            SelectObject(ps.hdc, penMap[colorCode]);
+            Rectangle(
+                ps.hdc,
+                x * SIZE,
+                y * SIZE,
+                x * SIZE + SIZE,
+                y * SIZE + SIZE
+            );
+        }
+    }
+
+    //DeleteObject(blackPen);
+    SelectObject(ps.hdc, original);
+}
+
 void FlyWeightState::handleInput(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam, Page* page)
 {
     switch (message)
@@ -19,7 +87,10 @@ void FlyWeightState::handleInput(HWND hWnd, UINT message, WPARAM wParam, LPARAM 
             HDC hdc = BeginPaint(hWnd, &ps);
 
             FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-            drawField(hWnd, ps);
+
+            World* world = new World();
+            world->generateTerrain();
+            world->drawTerrain(hWnd, ps);
 
             EndPaint(hWnd, &ps);
         }
--- GameProgrammingPatterns/GameProgrammingPatterns/FlyWeightState.h
+++ GameProgrammingPatterns/GameProgrammingPatterns/FlyWeightState.h
@@ -1,6 +1,42 @@
 #pragma once
 
 #include "PageState.h"
+#include <string>
+
+class Terrain
+{
+public:
+    Terrain(int moveCost, bool isWater, int color)
+    : moveCost_(moveCost),
+      isWater_(isWater),
+      color_(color)
+    {}
+    int getColor() const { return color_; };
+private:
+    int moveCost_;
+    bool isWater_;
+    int color_;
+};
+
+class World
+{
+public:
+    World()
+      : grassTerrain_(1, false, 0x42c436),
+      hillTerrain_(3, false, 0x1ad6e0),
+      riverTerrain_(2, true, 0xc48c2d)
+    {}
+    void generateTerrain();
+    void drawTerrain(HWND hWnd, PAINTSTRUCT ps);
+private:
+    static const int WIDTH = 10;
+    static const int HEIGHT = 10;
+    static const int SIZE = 32;
+    Terrain* tiles_[WIDTH][HEIGHT];
+    Terrain grassTerrain_;
+    Terrain hillTerrain_;
+    Terrain riverTerrain_;
+};
 
 class FlyWeightState : public PageState
 {
